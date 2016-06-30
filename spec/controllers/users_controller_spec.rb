f628e7decd9ec5935dec8e42876bdad7938160b2
andela-bmakinwa <makinwa37@gmail.com>
2016-06-30 15:15:21 +0100

    style: fix style violations raised by rubocop

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,11 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe UsersController, type: :request do
   describe "POST /api/users" do
     it "returns http success" do
-      post "/api/users", { users: attributes_for(:user) }
+      post "/api/users", users: attributes_for(:user)
       expect(response).to have_http_status(:created)
     end
   end
-
 end
