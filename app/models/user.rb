450bd9efe220ca7c28398c2097410955d0b5a341
andela-bmakinwa <makinwa37@gmail.com>
2016-07-02 07:21:35 +0100

    fix(user): update validation param for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
 
   validates :email, format: { with: VALID_EMAIL_FORMAT }
   validates :name, :username, :email, presence: true, length: { minimum: 3 }
-  validates :password, length: { minimum: 3 }
+  validates :password, presence: true, length: { minimum: 3 }
 end
