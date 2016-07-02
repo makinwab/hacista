6f37ccad51e65c768b6d0ad80769fe20402ba0d6
andela-bmakinwa <makinwa37@gmail.com>
2016-07-02 07:19:59 +0100

    test(user): write specs for user controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,32 @@
 require "rails_helper"
+require "json"
 
 RSpec.describe UsersController, type: :request do
   describe "POST /api/users" do
-    it "returns http success" do
-      post "/api/users", users: attributes_for(:user)
-      expect(response).to have_http_status(:created)
+    context "when user data is valid" do
+      it "returns users' data and http created status" do
+        valid_user = attributes_for(:user)
+        post "/api/users", users: valid_user
+        user = JSON.parse(response.body)
+
+        expect(user["name"]).to eql valid_user[:name]
+        expect(user["email"]).to eql valid_user[:email]
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context "when user data is invalid" do
+      it "returns error message and http error status" do
+        post "/api/users", users: attributes_for(:invalid_user)
+        messages = JSON.parse(response.body)
+        length_message = "is too short (minimum is 3 characters)"
+
+        expect(messages["name"]).to include "can't be blank"
+        expect(messages["email"]).to include "is invalid"
+        expect(messages["username"]).to include length_message
+        expect(messages["password"]).to include length_message
+        expect(response).to have_http_status(422)
+      end
     end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,12 @@ FactoryGirl.define do
     email "makinwa37@gmail.com"
     password "makinwa37"
     username "makinwa37"
+
+    factory :invalid_user do
+      name ""
+      email "makinwa37"
+      password "ma"
+      username "ma"
+    end
   end
 end
