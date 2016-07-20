fb414dea7a76d563aa22c0a4f01a28c75e702197
andela-bmakinwa <makinwa37@gmail.com>
2016-07-20 17:35:05 +0100

    chore(test): improve code quality

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -5,27 +5,39 @@ RSpec.describe SessionsController, type: :request do
     before(:each) { create(:user) }
 
     context "when the user exists" do
-      it "returns serialized user data and http status" do
-        valid_user = attributes_for(:valid_auth)
-        post "/api/users/auth", users: valid_user
+      before(:all) do
+        post "/api/users/auth", users: attributes_for(:valid_auth)
+      end
 
+      let(:valid_user) { attributes_for(:valid_auth) }
+
+      it "returns serialized user data" do
         user = JSON.parse(response.body)
         id64 = %r(^(?:[A-Za-z0-9+\/]{4}\n?)*(?:[A-Za-z0-9+\/]{2}==|
         [A-Za-z0-9+\/]{3}=)?$)
 
         expect(user["id"]).to match id64
         expect(user["email"]).to eql valid_user[:email]
+      end
+
+      it "returns a 200 http status code" do
         expect(response).to have_http_status(200)
       end
     end
 
     context "when the user does not exist" do
-      it "returns an error message and http status" do
-        invalid_user = attributes_for(:invalid_auth)
-        post "/api/users/auth", users: invalid_user
-        message = JSON.parse(response.body)
+      before(:all) do
+        post "/api/users/auth", users:  attributes_for(:invalid_auth)
+      end
 
+      let(:invalid_user) { attributes_for(:invalid_auth) }
+
+      it "returns an error message" do
+        message = JSON.parse(response.body)
         expect(message["error"]).to eql "invalid email or password"
+      end
+
+      it "returns a 404 http status code" do
         expect(response).to have_http_status(404)
       end
     end
